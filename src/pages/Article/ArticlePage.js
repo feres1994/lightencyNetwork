57c37528e7ee8152ef4b35d9ca7557d3d81cbd46
feres94 <feresfatnassi@gmail.com>
2019-12-24 10:48:31 +0100

    add 404 page

--- src/pages/Article/ArticlePage.js
+++ src/pages/Article/ArticlePage.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import { getFirebase } from "../../firebase";
+import { Link } from "react-router-dom";
 import Loader from "../../components/loader";
 import facebook from "../../graphics/facebook.png";
+import NotFound from "../../graphics/p404.png";
 import linkedin from "../../graphics/linkedin.png";
 import twitter from "../../graphics/twitter.png";
 import "./ArticlePage.scss";
 
+const RedirectingBtn = {
+  width: "150px",
+  padding: "7px",
+  height: "50px",
+  color: "white",
+  background: "black",
+  border: "black"
+};
 export default class ArticlePage extends Component {
   state = {
-    article: false
+    article: false,
+    error: "this article is not found"
   };
   componentDidMount() {
     getFirebase()
@@ -25,8 +36,8 @@ export default class ArticlePage extends Component {
   }
 
   render() {
-    const { article } = this.state;
-    return !article ? (
+    const { article, error } = this.state;
+    return article === false ? (
       <div
         style={{
           minHeight: "100vh",
@@ -37,12 +48,36 @@ export default class ArticlePage extends Component {
       >
         <Loader />
       </div>
+    ) : article === null ? (
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <img src={NotFound} />
+
+        <Link to="/blog">
+          <button style={RedirectingBtn}>Redirect to blog page</button>
+        </Link>
+      </div>
     ) : (
       <div className="ArticlePage" style={{ minHeight: "100vh" }}>
         <div className="ArticlePageWrapper">
           <div className="Article-title">{article.title}</div>
           <div className="Article-info">
-            Posted on {article.dateFormatted} by {article.author}{" "}
+            <p>
+              Posted on{" "}
+              <span style={{ fontWeight: "bold", width: "150px" }}>
+                {article.dateFormatted}
+              </span>{" "}
+              by{" "}
+              <span style={{ fontWeight: "bold", width: "150px" }}>
+                {article.author}
+              </span>{" "}
+            </p>
             <div className="social">
               <a href={article.fb} target="_blank">
                 <img src={facebook} alt="facebook" />
