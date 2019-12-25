eb9d027215b2b3dde8c60b3f1a92b3c49d58f2a9
Chaâri Karim <chaari.karim.plus@gmail.com>
2019-12-25 17:18:21 +0100

    changed router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route  } from "react-router-dom";
 
 import Routes from "./Routes";
 
 function AppRouter(props) {
   return (
-    <Router>
+    <HashRouter>
       <Route path="/" component={Routes} />
-    </Router>
+    </HashRouter>
   );
 }
 
