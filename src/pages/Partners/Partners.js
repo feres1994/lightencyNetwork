5f53ca4e34cdd4c210d22f9acb5fa2f6eb6c0081
feres94 <feresfatnassi@gmail.com>
2020-03-05 15:00:13 +0100

    add yele aura partners

--- src/pages/Partners/Partners.js
+++ src/pages/Partners/Partners.js
@@ -1,19 +1,21 @@
-import React from "react";
-import partner_1 from "../../graphics/P.png";
-import partner_2 from "../../graphics/P1.png";
-import partner_3 from "../../graphics/P2.png";
-import partner_4 from "../../graphics/p4.png";
-import partner_6 from "../../graphics/p6.png";
-import partner_7 from "../../graphics/P7.png";
-import partner_8 from "../../graphics/p9.png";
-import partner_9 from "../../graphics/p10.png";
-import partner_10 from "../../graphics/p12.png";
-import partner_11 from "../../graphics/p13.png";
-import partner_12 from "../../graphics/p14.png";
-import partner_13 from "../../graphics/p15.png";
-import partner_14 from "../../graphics/p16.png";
+import React from 'react';
+import partner_1 from '../../graphics/P.png';
+import partner_2 from '../../graphics/P1.png';
+import partner_3 from '../../graphics/P2.png';
+import partner_4 from '../../graphics/p4.png';
+import partner_6 from '../../graphics/p6.png';
+import partner_7 from '../../graphics/P7.png';
+import partner_8 from '../../graphics/p9.png';
+import partner_9 from '../../graphics/p10.png';
+import partner_10 from '../../graphics/p12.png';
+import partner_11 from '../../graphics/p13.png';
+import partner_12 from '../../graphics/p14.png';
+import partner_13 from '../../graphics/p15.png';
+import partner_14 from '../../graphics/p16.png';
+import partner_15 from '../../graphics/aura.png';
+import partner_16 from '../../graphics/yele.png';
 
-import "./Partners.scss";
+import './Partners.scss';
 
 const partnerTab = [
   partner_1,
@@ -28,7 +30,9 @@ const partnerTab = [
   partner_11,
   partner_12,
   partner_13,
-  partner_14
+  partner_14,
+  partner_15,
+  partner_16
 ];
 
 class Partners extends React.Component {
